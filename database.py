57b0e6e9561d8b1986f2910f5939d0163a8d9c65
junpark97 <junpark.business@gmail.com>
2018-02-17 17:18:05 -0500

    Created Database
    Returns list of points (long,lat,message)

--- database.py
+++ database.py
@@ -1,22 +1,62 @@
 # Database to store and fetch the messages
 import json
 from point import Point
+import os
+import sqlite3
 
 class Database:
-
+  
   def __init__(self):
-    # A "database" until we have an actual database
-    self.fakeDatabase = [
-      Point(43.472113, -80.543936, 'Hello world!'),
-      Point(43.471772, -80.545337, 'thank mr goose')
-    ]
+    """Connects to the specific database."""    
+    # A "database" until we have an actual database; connect to the SQL database
+    print("Init")
 
-  def create(self, lat, long, message):
-    point = Point(lat, long, message)
+  def create(self, db, lat, long, message):
+#    point = Point(lat, long, message)
+#    print("[Database] Created new: " + str(point))
+    #self.fakeDatabase.append(point) # Inserts
+    db.execute('insert into points (message, long, lat) values (?, ?, ?)',
+                 [message,long,lat])
+    db.commit()
 
-    print("[Database] Created new: " + str(point))
-    self.fakeDatabase.append(point)
 
-  def query(self, lat, long, limit=10):
+  def query(self, db, lat, long, limit=10):
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
     # This just converts all the Points to a JSON form before sending it back
-    return json.dumps(map(lambda p: p.toJSON(), self.fakeDatabase))
+    cur = db.execute('select lat, long, message from points order by id desc') #order by newest to oldest?
+    points = cur.fetchall()
+    entries_list = []
+    x = 0
+    def loop_1(point):
+        entries_list = []
+        x = 0
+        while x < len(point):
+            entries_list.append(str(Point(point[x][0],point[x][1],point[x][2])))
+            x = x + 1
+        return entries_list
+    return loop_1(points)
+    #return str(points[0][0]) + ", " + str(points[0][1]) + ", " + points[0][2]
+    print("Show entries")
+
+    # return json.dumps(map(lambda p: p.toJSON(), self.fakeDatabase)) #Selects
+  
+  def close_db(self):
+    """Closes the database again at the end of the request."""
+    #if hasattr(g, 'sqlite_db'):
+    #    g.sqlite_db.close()
+    print("Close db")
+
+  #def show_entries(self, db):
+  #  cur = db.execute('select title, text from entries order by id desc')
+  #  entries = cur.fetchall()
+  #  return entries
+  #  print("Show entries")
+
+  # def add_entry(self, db):
+  #   if not session.get('logged_in'):
+  #       abort(401)
+  #   db.execute('insert into entries (title, text) values (?, ?)',
+  #                [request.form['title'], request.form['text']])
+  #   db.commit()
--- server.py
+++ server.py
@@ -1,9 +1,28 @@
-from flask import Flask, request
+#imports
+import os
+import sqlite3
+
+from flask import Flask, request, g
 import json
 from point import Point
 from database import Database
 
 app = Flask(__name__)
+app.config.from_object(__name__) # load config from this file  ****
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'database.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def get_sqlite():
+  rv = sqlite3.connect(app.config['DATABASE'])
+  rv.row_factory = sqlite3.Row
+  return rv
 
 db = Database()
 
@@ -16,17 +35,37 @@ def getPoints():
   long = request.args.get('long', 0)
 
   print('[Server] Getting messages for (' + str(lat) + ', ' + str(long) + ')')
-  return db.query(lat, long)
+  return str(db.query(get_sqlite(), lat, long))
 
 # Create a new message at the location
 @app.route('/messages', methods=['POST'])
 def createPoint():
   # We should sanity check the JSON here before adding to the database
   print(request.json)
-  db.create(request.json['lat'], request.json['long'], request.json['message'])
+  db.create(get_sqlite(), request.json['lat'], request.json['long'], request.json['message'])
   return 'Successfully created message'
 
 # Test to see if the server is up
 @app.route('/ping', methods=['GET'])
 def ping():
   return 'Pong! :)'
+
+@app.teardown_appcontext
+def close_db(error):
+  db.close_db()
+  
+@app.cli.command('initdb')
+def initdb_command():
+  with app.open_resource('schema.sql', mode='r') as f:
+    db = get_sqlite()
+    db.cursor().executescript(f.read())
+    db.commit()
+    print('Initialized the database.')
+  
+# @app.route('/')
+# def show_entries():
+#   return str(db.show_entries(get_sqlite()))
+
+# @app.route('/add', methods=['POST'])
+# def add_entry():
+#   db.add_entry()
